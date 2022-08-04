140d4841a63f33ed0ac3c7cd3d1cb998b6afc9a1
EmersonSalcedo <emersondavid501@gmail.com>
2022-08-03 20:47:10 -0500

    first commit

--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -6,20 +6,33 @@ import '../../data/models/random_user_model.dart';
 import '../entities/random_user.dart';
 
 class UserRepository {
-
-
+  late UserRemoteDatatasource userRemoteDatatasource;
+  late UserLocalDataSource userLocalDataSource;
   UserRepository() {
     logInfo("Starting UserRepository");
+    userRemoteDatatasource = UserRemoteDatatasource();
+    userLocalDataSource = UserLocalDataSource();
   }
 
   Future<bool> getUser() async {
+    RandomUser randomUser = await userRemoteDatatasource.getUser();
+    await userLocalDataSource.addUser(randomUser);
+    return Future.value(true);
   }
 
-  Future<List<RandomUser>> getAllUsers() async =>
+  Future<List<RandomUser>> getAllUsers() async {
+    return userLocalDataSource.getAllUsers();
+  }
 
-  Future<void> deleteUser(id) async =>
+  Future<void> deleteUser(id) async {
+    await userLocalDataSource.deleteUser(id);
+  }
 
-  Future<void> deleteAll() async => 
+  Future<void> deleteAll() async {
+    await userLocalDataSource.deleteAll();
+  }
 
-  Future<void> updateUser(user) async => 
+  Future<void> updateUser(user) async {
+    await userLocalDataSource.updateUser(user);
+  }
 }
